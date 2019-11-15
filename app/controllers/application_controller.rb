bf54c0d0775ae292afc62f6434c2f6b3b1c1ed02
ruffini47 <ruffini47@gmail.com>
2019-11-15 14:16:21 +0000

    attendance-B-month<->week_日付重複かなりいい感じ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -94,9 +94,9 @@ class ApplicationController < ActionController::Base
             #for n in 1..@user.attendances.count do
               #if day != @user.attendances.find(n).worked_on
                 if @month == false
-                  if day < @first_day_w             
-                  @user.attendances.create!(worked_on: day)
-                  @user.attendances = @user.attendances.distinct
+                  if [day, @first_day].compact.min == day && day != @first_day   #if day < @first_day_m なら
+                    @user.attendances.create!(worked_on: day)
+                    @user.attendances = @user.attendances.distinct
                   end
                 else
                   @user.attendances.create!(worked_on: day)
