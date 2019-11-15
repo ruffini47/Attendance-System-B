e09017028b866d15fe5089820ec829fd090aae25
ruffini47 <ruffini47@gmail.com>
2019-11-15 08:08:30 +0000

    attendance-B-month<->week_日付重複あり

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,10 +39,33 @@ class ApplicationController < ActionController::Base
   # beforeフィルター
     
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
-  def set_one_month 
-    @first_day = params[:date].nil? ?
-    Date.current.beginning_of_month : params[:date].to_date
-    @last_day = @first_day.end_of_month
+  def set_one_month
+    if params[:month].nil?
+      if params[:date].nil?
+        @first_day = Date.current.beginning_of_month 
+      else
+        @first_day = params[:date].to_date.beginning_of_month
+      end
+      @last_day = @first_day.end_of_month
+      @month = true
+    elsif params[:month] == "true"
+      if params[:date].nil?
+        @first_day = Date.current.beginning_of_month
+      else 
+        @first_day = params[:date].to_date.beginning_of_month
+      end
+      @last_day = @first_day.end_of_month
+      @month = true
+    elsif params[:month] == "false"
+      if params[:date].nil?
+        @first_day = Date.current.beginning_of_week
+      else
+        @first_day= params[:date].to_date.beginning_of_week
+      end
+      @last_day = @first_day.end_of_week
+      @month = false
+    else
+    end
     one_month = [*@first_day..@last_day] # 対象の月の日数を代入します。
     # ユーザーに紐付く一ヶ月分のレコードを検索し取得します。
     @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
@@ -54,9 +77,23 @@ class ApplicationController < ActionController::Base
       end
       @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     end
-
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
     flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
     redirect_to root_url
   end
+  
+  def set_month
+    @month = true
+    @first_day = params[:date]
+    redirect_to user_url(date: @first_day, month: @month)
+  end
+  
+  def set_week
+    @month = false
+    @first_day = params[:date]
+    redirect_to user_url(date: @first_day, month: @month)
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
       patch 'update_basic_info'
       get 'attendances/edit_one_month'
       patch 'attendances/update_one_month'
+      get 'attendances/set_week'
+      get 'attendances/set_month'
     end
     resources :attendances, only: :update
   end
