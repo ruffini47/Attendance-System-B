9c91044e5dbe7c21a349db4fc3522d34d1b44a19
ruffini47 <ruffini47@gmail.com>
2019-11-16 08:03:55 +0000

    attendance-B-month<->week_リファクタリング後まだおかしい特にweek_←

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,46 +40,60 @@ class ApplicationController < ActionController::Base
     
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
   def set_one_month
-    if params[:month].nil?
-      if params[:date].nil?
-        @first_day_m = Date.current.beginning_of_month 
-      else
-        @first_day_m = params[:date].to_date.beginning_of_month
-      end
-      @last_day_m = @first_day_m.end_of_month
+#    if params[:month].nil?
+#      if params[:date].nil?
+#        @first_day = Date.current.beginning_of_month 
+#      else
+#        @first_day = params[:date].to_date.beginning_of_month
+#      end
+#      @last_day = @first_day.end_of_month
+#      @month = true
+#    elsif params[:month] == "true"
+#      if params[:date].nil?
+#        @first_day = Date.current.beginning_of_month
+#      else 
+#        @first_day = params[:date].to_date.beginning_of_month
+#      end
+#      @last_day = @first_day.end_of_month
+#      @month = true
+#    elsif params[:month] == "false"
+#      if params[:date].nil?
+#        @first_day = Date.current.beginning_of_week
+#      else
+#        @first_day= params[:date].to_date.beginning_of_week
+#      end
+#      @last_day = @first_day.end_of_week
+#      @month = false
+#    else
+#    end
+#    if @month == true
+#      @first_day = @first_day_m
+#      @last_day = @last_day_m
+#    else
+#      @first_day = @first_day_w
+#      @last_day = @last_day_w
+#    end
+
+    if params[:date].nil? || params[:month].nil?
+      @first_day = Date.current.beginning_of_month
+      @last_day = @first_day.end_of_month
       @month = true
     elsif params[:month] == "true"
-      if params[:date].nil?
-        @first_day_m = Date.current.beginning_of_month
-      else 
-        @first_day_m = params[:date].to_date.beginning_of_month
-      end
-      @last_day_m = @first_day_m.end_of_month
+      @first_day = params[:date].to_date.beginning_of_month
+      @last_day = @first_day.end_of_month
       @month = true
     elsif params[:month] == "false"
-      if params[:date].nil?
-        @first_day_w = Date.current.beginning_of_week
-      else
-        @first_day_w= params[:date].to_date.beginning_of_week
-      end
-      @last_day_w = @first_day_w.end_of_week
+      @first_day = params[:date].to_date.beginning_of_week
+      @last_day = @first_day.end_of_week
       @month = false
-    else
-    end
-    if @month == true
-      @first_day = @first_day_m
-      @last_day = @last_day_m
-    else
-      @first_day = @first_day_w
-      @last_day = @last_day_w
     end
     one_month = [*@first_day..@last_day] # 対象の月の日数を代入します。  
     # ユーザーに紐付く一ヶ月分のレコードを検索し取得します。
     @attendances = nil
-    #if @user.attendances.count != 0
+    if @user.attendances.count != 0
       @user.attendances.each {
         |a| a = nil}
-    #end
+    end
     @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
     
     #if @month == false
@@ -94,11 +108,29 @@ class ApplicationController < ActionController::Base
             #for n in 1..@user.attendances.count do
               #if day != @user.attendances.find(n).worked_on
                 if @month == false
-                  if [day, @first_day].compact.min == day && day != @first_day   #if day < @first_day_m なら
+                  if [day, @first_day].compact.min == day && day != @first_day
+                    #if day < @first_day なら
                     @user.attendances.create!(worked_on: day)
                     @user.attendances = @user.attendances.distinct
                   end
-                else
+                  if @first_day.month != @last_day.month
+                    if[day, @last_day.beginning_of_month].compact.min == day && day != @last_day.beginning_of_month 
+                      # if day <  @last_day.beginning_of_month なら
+                      @user.attendances.create!(worked_on: day)
+                      @user.attendances = @user.attendances.distinct
+                    end
+                  elsif @first_day.month == @last_day.month
+                    if day >= @first_day && day <= @last_day
+                      # if @first_day <= day <= @last_day
+                      @user.attendances.each do |d|
+                        if d.worked_on != day
+                          @user.attendances.create!(worked_on: day)
+                          @user.attendances = @user.attendances.distinct
+                        end
+                      end
+                    end
+                  end
+                elsif @month == true
                   @user.attendances.create!(worked_on: day)
                   @user.attendances = @user.attendances.distinct
                 end
@@ -117,18 +149,26 @@ class ApplicationController < ActionController::Base
     redirect_to root_url
   end
   
-  def set_month
-    @month = true
-    @first_day = params[:date]
-    redirect_to user_url(date: @first_day, month: @month)
-  end
+  #def set_month
+  #  @first_day = params[:date].nil? ?
+  #  Date.current.beginning_of_month : params[:date].to_date.beginning_of_month
+  #  redirect_to user_url(date: @first_day, month: params[:month])
+  #end
+    
+    
+#    @month = true
+#    @first_day = params[:date].to_date.beginning_of_month
+#    redirect_to user_url(date: @first_day, month: @month)
+#  end
   
-  def set_week
-    @month = false
-    @first_day = params[:date]
-    redirect_to user_url(date: @first_day, month: @month)
-  end
+  #def set_week
+  #  @first_day = params[:date].nil? ?
+  #  Date.current.beginning_of_week : params[:date].to_date.beginning_of_week
+  #  redirect_to user_url(date: @first_day, month: params[:month])  
+  #end
 
-
+#    @month = false
+#    @first_day = params[:date].to_date.beginning_of_week
+#    redirect_to user_url(date: @first_day, month: @month)
 
 end
