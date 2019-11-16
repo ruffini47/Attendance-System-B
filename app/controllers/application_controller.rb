ef6f5270b2da70004953baf2ebd777ae084be1e9
ruffini47 <ruffini47@gmail.com>
2019-11-16 13:47:07 +0000

    attendance-B-month<->week_週表示のみ月表示のみでOK_一部リファクタリング前

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -114,10 +114,20 @@ class ApplicationController < ActionController::Base
                     @user.attendances = @user.attendances.distinct
                   end
                   if @first_day.month != @last_day.month
-                    if[day, @last_day.beginning_of_month].compact.min == day && day != @last_day.beginning_of_month 
+#                    if[day, @last_day.beginning_of_month].compact.min == day && day != @last_day.beginning_of_month 
                       # if day <  @last_day.beginning_of_month なら
-                      @user.attendances.create!(worked_on: day)
-                      @user.attendances = @user.attendances.distinct
+                    if day >= @first_day && day <= @last_day
+                      # if @first_day <= day <= @last_day
+                      hit = false
+                      @user.attendances.each do |d|
+                        if day == d.worked_on
+                            hit = true
+                        end
+                      end
+                      if hit == false
+                        @user.attendances.create!(worked_on: day)
+                        @user.attendances = @user.attendances.distinct
+                      end
                     end
                   elsif @first_day.month == @last_day.month
                     if day >= @first_day && day <= @last_day
